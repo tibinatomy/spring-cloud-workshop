b982ade2ad7f7c882d33be342ca64551abdc1ba1
Tibin A T <Tibin.Tomy@ibsplc.com>
2023-08-11 16:12:25 +0530

    initial commit

---
+++ usermanager/user360service/src/main/java/com/tibs/spring/cloud/workshop/usermanager/User360ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tibs.spring.cloud.workshop.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User360ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(User360ServiceApplication.class, args);
+	}
+
+}
---
+++ usermanager/user360service/src/test/java/com/tibs/spring/cloud/workshop/usermanager/User360managerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibs.spring.cloud.workshop.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class User360managerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
